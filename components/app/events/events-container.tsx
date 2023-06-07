09cde033948cb2283802c8082441cbdaf9a9afbd
dimfu <0arcetros@gmail.com>
2023-06-07 13:42:48 +0800

    refactor: move event sorter to api route

--- components/app/events/events-container.tsx
+++ components/app/events/events-container.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import moment from 'moment-timezone'
 import { EventItem, EventSkeleton } from './event-item'
 import type { Event } from '@/infrastructure/event'
 import { useUserContext } from '@/components/shared/providers/user-context'
@@ -10,45 +9,31 @@ interface Props {
 }
 
 export default function EventsContainer({ isLoading, events }: Props) {
-  const { timezone, hidePastEvents } = useUserContext()
+  const { hidePastEvents } = useUserContext()
   return (
     <ul className="flex flex-col items-center justify-between mt-[25px] space-y-4 pb-5 w-full lg:w-2/3">
       {!isLoading
-        ? events
-          ?.sort((a, b) => {
-            const today = moment().tz(timezone)
-            const dateA = moment.utc(a.schedule.at(-1)?.content.at(-1)?.time.split('–')[1] || a.schedule.at(-1)?.content.at(-1)?.time || a.startDate).tz(timezone)
-            const dateB = moment.utc(b.schedule.at(-1)?.content.at(-1)?.time.split('–')[1] || b.schedule.at(-1)?.content.at(-1)?.time || b.startDate).tz(timezone)
-
-            if (dateA.isBefore(today) && dateB.isBefore(today))
-              return dateB.diff(dateA)
-            else if (dateA.isBefore(today))
-              return 1
-            else if (dateB.isBefore(today))
-              return -1
-            else return dateA.diff(dateB)
-          })
-          ?.map(event => (
-              <EventItem
-                key={event.url}
-                event={event}
-                hide={hidePastEvents}
-                details={
-                  <EventItem.EventDetailsWrapper>
-                    <EventItem.Name />
-                    <EventItem.Details />
-                  </EventItem.EventDetailsWrapper>
-                }
-                actions={
-                  <React.Fragment>
-                    <EventItem.Schedule/>
-                    <EventItem.Link href={event.url}>
-                      <span className="font-semibold">Learn More</span>
-                    </EventItem.Link>
-                  </React.Fragment>
-                }
-              />
-          ))
+        ? events?.map(event => (
+            <EventItem
+              key={event.url}
+              event={event}
+              hide={hidePastEvents}
+              details={
+                <EventItem.EventDetailsWrapper>
+                  <EventItem.Name />
+                  <EventItem.Details />
+                </EventItem.EventDetailsWrapper>
+              }
+              actions={
+                <React.Fragment>
+                  <EventItem.Schedule />
+                  <EventItem.Link href={event.url}>
+                    <span className="font-semibold">Learn More</span>
+                  </EventItem.Link>
+                </React.Fragment>
+              }
+            />
+        ))
         : Array.from({ length: 8 }).map((_, id) => <EventSkeleton key={id} />)}
     </ul>
   )
--- pages/api/events.ts
+++ pages/api/events.ts
@@ -1,12 +1,28 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import Fuse from 'fuse.js'
+import moment from 'moment-timezone'
 import { d1, dmec, fdUs, lzWorldTour } from '@/_data'
 import type { Event } from '@/infrastructure/event'
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const { search } = req.query
   const mergeEvents = [...dmec, ...fdUs, ...lzWorldTour, ...d1]
-  const fuseInstance = new Fuse<Event>(mergeEvents, {
+
+  const sortSoonFirst = mergeEvents.sort((a, b) => {
+    const today = moment.utc()
+    const dateA = moment.utc(a.schedule.at(-1)?.content.at(-1)?.time.split('–')[1] || a.schedule.at(-1)?.content.at(-1)?.time || a.startDate)
+    const dateB = moment.utc(b.schedule.at(-1)?.content.at(-1)?.time.split('–')[1] || b.schedule.at(-1)?.content.at(-1)?.time || b.startDate)
+
+    if (dateA.isBefore(today) && dateB.isBefore(today))
+      return dateB.diff(dateA)
+    else if (dateA.isBefore(today))
+      return 1
+    else if (dateB.isBefore(today))
+      return -1
+    else return dateA.diff(dateB)
+  })
+
+  const fuseInstance = new Fuse<Event>(sortSoonFirst, {
     keys: [
       { name: 'name', weight: 1 },
       { name: 'slug', weight: 0.5 },
