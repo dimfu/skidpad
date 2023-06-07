dc2466e8cf087fabf611df8a787b9acfad272c58
dimfu <0arcetros@gmail.com>
2023-06-07 22:53:45 +0800

    refactor: forgot to remove schema tag in container

--- components/app/events/events-container.tsx
+++ components/app/events/events-container.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { EventItem, EventSkeleton } from './event-item'
 import type { Event } from '@/infrastructure/event'
 import { useUserContext } from '@/components/shared/providers/user-context'
-import { schema } from '@/_data'
 
 interface Props {
   isLoading: boolean
@@ -12,7 +11,9 @@ interface Props {
 export function EventsContainerSkeleton() {
   return (
     <ul className="flex flex-col items-center justify-between mt-[25px] space-y-4 pb-5 w-full lg:w-2/3">
-      {Array.from({ length: 8 }).map((_, id) => <EventSkeleton key={id} />)}
+      {Array.from({ length: 8 }).map((_, id) => (
+        <EventSkeleton key={id} />
+      ))}
     </ul>
   )
 }
@@ -23,29 +24,25 @@ export default function EventsContainer({ isLoading, events }: Props) {
     <ul className="flex flex-col items-center justify-between mt-[25px] space-y-4 pb-5 w-full lg:w-2/3">
       {!isLoading
         ? events?.map(event => (
-            <>
-              <EventItem
-                key={event.url}
-                event={event}
-                hide={hidePastEvents}
-                details={
-                  <EventItem.EventDetailsWrapper>
-                    <EventItem.Name />
-                    <EventItem.Details />
-                  </EventItem.EventDetailsWrapper>
-                }
-                actions={
-                  <React.Fragment>
-                    <EventItem.Schedule />
-                    <EventItem.Link href={event.url}>
-                      <span className="font-semibold">Learn More</span>
-                    </EventItem.Link>
-                  </React.Fragment>
-                }
-              />
-
-              <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(schema(events)) }} />
-            </>
+            <EventItem
+              key={event.url}
+              event={event}
+              hide={hidePastEvents}
+              details={
+                <EventItem.EventDetailsWrapper>
+                  <EventItem.Name />
+                  <EventItem.Details />
+                </EventItem.EventDetailsWrapper>
+              }
+              actions={
+                <React.Fragment>
+                  <EventItem.Schedule />
+                  <EventItem.Link href={event.url}>
+                    <span className="font-semibold">Learn More</span>
+                  </EventItem.Link>
+                </React.Fragment>
+              }
+            />
         ))
         : Array.from({ length: 8 }).map((_, id) => <EventSkeleton key={id} />)}
     </ul>
