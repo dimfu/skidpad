9b507413195c1456689ea867b4fde56322e0f4dd
dimfu <0arcetros@gmail.com>
2023-06-08 12:39:16 +0800

    fix: revert to client timezone sorting

--- pages/api/events.ts
+++ pages/api/events.ts
@@ -1,12 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import Fuse from 'fuse.js'
-import { sortSoonFirst } from '@/_data'
 import type { Event } from '@/infrastructure/event'
+import { mergeEvents } from '@/_data'
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const { search } = req.query
 
-  const fuseInstance = new Fuse<Event>(sortSoonFirst, {
+  const fuseInstance = new Fuse<Event>(mergeEvents, {
     keys: [
       { name: 'name', weight: 1 },
       { name: 'slug', weight: 0.5 },
@@ -17,6 +17,6 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
     return res.status(200).json(fusedEvents.map(event => event.item))
   }
   else {
-    return res.status(200).json(sortSoonFirst)
+    return res.status(200).json(mergeEvents)
   }
 }
