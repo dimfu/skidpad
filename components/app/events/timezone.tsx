c32100add86d39333b40306a445ea5b2ebbd844b
dimfu <0arcetros@gmail.com>
2023-06-25 11:02:36 +0800

    feat(timezone): improve timezone select options

--- components/app/events/timezone.tsx
+++ components/app/events/timezone.tsx
@@ -27,6 +27,26 @@ export default function Timezone() {
   const { timezone, updateTimezone } = useUserContext()
   const timezones = getAllTimezones()
 
+  const scrubbedPrefixes = [
+    'Antarctica',
+    'Arctic',
+    'Canada',
+    'Chile',
+    'Etc',
+    'Mexico',
+    'US',
+  ]
+
+  const timezoneKeys = Object.keys(timezones).filter(name => name.includes('/')).filter(name => !scrubbedPrefixes.includes(name.split('/')[0]))
+  const timezoneItems = timezoneKeys.reduce((memo: { name: string; offset: number; offsetString: string }[], tz) => {
+    memo.push({
+      name: tz,
+      offset: timezones[tz as keyof typeof timezones].utcOffset,
+      offsetString: timezones[tz as keyof typeof timezones].utcOffsetStr,
+    })
+    return memo
+  }, []).sort((a, b) => a.offset - b.offset)
+
   function resetTimezone() {
     updateTimezone(moment.tz.guess())
   }
@@ -36,21 +56,19 @@ export default function Timezone() {
       <div className="flex items-center justify-between mb-2">
         <h2 className="text-sm text-neutral-400">Select Timezone</h2>
         <div onClick={resetTimezone} className='flex items-center space-x-1 group text-sm text-neutral-400 cursor-pointer'>
-          <Repeat className='w-4 h-4'/>
+          <Repeat className='w-4 h-4' />
           <span className='group-hover:underline'>Reset</span>
         </div>
       </div>
       <Select.Root value={timezone} onValueChange={event => updateTimezone(event)}>
-        <Select.Trigger className="flex justify-between items-center rounded bg-[#1e1e1e] px-4 py-3 outline-none leading-none w-full">
-          <div className="flex items-center space-x-2">
+        <Select.Trigger className="flex flex-shrink justify-between items-center rounded text-left bg-[#1e1e1e] px-4 py-3 outline-none leading-none w-full">
+          <div className="flex items-center space-x-2 overflow-hidden">
             <Select.Icon>
               <Clock className="w-5 h-5 text-neutral-400" />
             </Select.Icon>
-            <Select.Value
-              aria-label="timezone"
-              placeholder={timezone}
-              className="inline-flex items-center justify-center rounded px-[15px] text-sm leading-none text-white shadow-[0_2px_10px] shadow-black/10 focus:shadow-[0_0_0_2px] focus:shadow-black data-[placeholder]:text-white outline-none"
-            />
+            <div className='whitespace-nowrap overflow-hidden text-ellipsis'>
+              <Select.Value aria-label="timezone" placeholder={timezone} />
+            </div>
           </div>
           <Select.Icon>
             <ChevronDown className="w-5 h-5 text-neutral-400" />
@@ -63,10 +81,10 @@ export default function Timezone() {
             </Select.ScrollUpButton>
             <Select.Viewport className="p-[5px] max-h-48">
               <Select.Group className="p-1 flex flex-col space-y-1">
-                {Object.keys(timezones).map((item, id) => (
-                  <Select.Item className="rounded cursor-pointer p-2 hover:bg-neutral-700 data-[state=checked]:bg-neutral-700" key={id} value={item}>
-                    <Select.ItemText className="text-sm">{item}</Select.ItemText>
-                    <Select.ItemIndicator className="absolute right-4 inline-flex items-center justify-center">
+                {timezoneItems.map((item, id) => (
+                  <Select.Item className="rounded cursor-pointer p-2 hover:bg-neutral-700 data-[state=checked]:bg-neutral-700" key={id} value={item.name}>
+                    <Select.ItemText><span className='text-sm truncate block'>(GMT{item.offsetString}) {item.name.replace('_', ' ')}</span></Select.ItemText>
+                    <Select.ItemIndicator className="absolute right-4 inline-flex self-center justify-center">
                       <Check className="w-5 h-5 text-neutral-400" />
                     </Select.ItemIndicator>
                   </Select.Item>
